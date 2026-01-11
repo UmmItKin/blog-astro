5cad77b7cc405e9b8d6d7efa228475f384d41ea8
UmmIt <git@ummit.dev>
2026-01-12 03:21:17 +0800

    chore: update dependencies and improve prose styles
    Fix the broken CSS issue.

--- uno.config.ts
+++ uno.config.ts
@@ -2,9 +2,8 @@ import {
   defineConfig,
   presetAttributify,
   presetIcons,
-  presetTypography,
-  presetUno,
   presetWebFonts,
+  presetWind3,
   transformerDirectives,
   transformerVariantGroup,
 } from 'unocss'
@@ -15,20 +14,20 @@ export default defineConfig({
       'bg-main': 'bg-hex-eef5fc dark:bg-hex-0d1117',
       'text-main': 'text-hex-555555 dark:text-hex-bbbbbb',
       'text-link': 'text-dark dark:text-white ',
-      'border-main': 'border-truegray-300 dark:border-truegray-600',
+      'border-main': 'border-neutral-300 dark:border-neutral-600',
     },
     {
       'text-title': 'text-link text-4xl font-800',
       'nav-link': 'text-link opacity-70 hover:opacity-100 transition-opacity duration-200 cursor-pointer',
-      'prose-link': 'text-link text-nowrap cursor-pointer border-b-1 !border-opacity-30 hover:!border-opacity-100 border-neutral-500 hover:border-truegray-600 dark:border-neutral-500 hover:dark:border-truegray-400 transition-border-color duration-200 decoration-none',
-      'container-link': 'p-2 opacity-60 hover:opacity-100 cursor-pointer hover:bg-truegray-500 !bg-opacity-10 transition-colors transition-opacity duration-200',
+      'prose-link': 'text-link text-nowrap cursor-pointer border-b-1 !border-opacity-30 hover:!border-opacity-100 border-neutral-500 hover:border-neutral-600 dark:border-neutral-500 hover:dark:border-neutral-400 transition-border-color duration-200 decoration-none',
+      'container-link': 'p-2 opacity-60 hover:opacity-100 cursor-pointer hover:bg-neutral-500 !bg-opacity-10 transition-colors transition-opacity duration-200',
     },
     {
-      'hr-line': 'w-14 mx-auto my-8 border-solid border-1px !border-truegray-200 !dark:border-truegray-800',
+      'hr-line': 'w-14 mx-auto my-8 border-solid border-1px !border-neutral-200 !dark:border-neutral-800',
     },
   ],
   presets: [
-    presetUno(),
+    presetWind3(),
     presetAttributify(),
     presetIcons({
       scale: 1.2,
@@ -37,7 +36,6 @@ export default defineConfig({
         display: 'inline-block',
       },
     }),
-    presetTypography(),
     presetWebFonts({
       fonts: {
         sans: 'Inter:400,600,800',
